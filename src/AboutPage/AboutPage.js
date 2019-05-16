b8f31f3a30f5b72fb34e26ff336b8dbc755e7a8e
breyerjs <breyer.js@gmail.com>
2019-05-16 09:25:59 -0700

    Fiddle with verbiage

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
         <div>
             <Header subheader={"is a software engineer"} />
             <p>I'm Jackson Breyer and this is my personal website.</p> 
-            <p>I'm currently a full-stack developer on the Search Experience team at Yelp. That means I write all sorts of code, and that it gets used by all sorts of people.</p>
+            <p>I'm currently a full-stack developer on the Search Experience team at Yelp. That means I write all sorts of code, and it gets used by all sorts of people.</p>
             <p>I wasn't always a Software Engineer. If you're interested in that journey, check out the 'History' section.</p>
             <p>Otherwise, the 'Technical' and 'Projects' pages may be what you're after.</p>
         </div>
