e9998fa00d75ae69e087f6a9c32de9ccdd3cd593
breyerjs <breyer.js@gmail.com>
2019-06-01 11:35:54 -0700

    Fix text

--- src/MainSite/LandingPage/LandingPage.js
+++ src/MainSite/LandingPage/LandingPage.js
@@ -9,12 +9,8 @@ const LandingPage = (props) => {
             <Header titleText={'Jackson Breyer'} subheader={"is a software engineer"} />
             <p>My name is Jackson Breyer and this is my personal website.</p> 
             <p>I'm a full-stack developer on the Search Experience team at Yelp. That means I write all sorts of code, and it gets used by all sorts of people.</p>
-            {props.fullSizeScreen &&
-                <div>
-                    <p>I wasn't always a Software Engineer. If you're interested in that journey, check out the 'History' section.</p>
-                    <p>Otherwise, the 'Technical' and 'Projects' pages may be what you're after.</p>            
-                </div>
-            }
+            <p>I wasn't always a Software Engineer. If you're interested in that journey, check out the 'History' section.</p>
+            <p>Otherwise, the 'Technical' and 'Projects' pages may be what you're after.</p>            
         </Layout>
     );
 };
