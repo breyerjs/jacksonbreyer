bc32debe2b61d887e3709a8258ace3b5bba7de9d
breyerjs <breyer.js@gmail.com>
2020-08-11 21:03:28 -0700

    copy

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <Header titleText={'Jackson Breyer'} subheader={"is an engineering manager"} />
         <Layout>
             <p>Hello. My name is Jackson Breyer and this is my personal website.</p> 
-            <p>I'm an Engineering Manager at Yelp, where I lead the team responsible for Search Experience, across Android, iOS and Web. When I'm not doing that, I enjoy writing, hiking, martial arts, and a host of other hobbies.</p>
+            <p>I'm an Engineering Manager at Yelp. There, I lead the team responsible for Search Experience, across Android, iOS and Web. When I'm not doing that, I enjoy writing, hiking, martial arts, and a host of other hobbies.</p>
             <p>Thanks for stopping by my corner of the Internet.</p>
         </Layout>
     </div>
