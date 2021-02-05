bf5f2cff680d7bfb78d524eaa1619736f2c0199a
breyerjs <breyer.js@gmail.com>
2021-02-04 23:47:54 -0800

    fix fout

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import Link from "next/link";
 import React from 'react';
 
 class CustomDocument extends Document {
@@ -6,10 +7,19 @@ class CustomDocument extends Document {
     return (
       <html lang="en">
         <Head>
-            <meta 
-                name="Description" 
-                content="The personal website of Mr. Jackson Breyer, software engineer and human being." 
-            />
+          {/* Load this font early since it isn't used until later on */}
+          <link 
+              rel="preload" 
+              href="/font/Spectral/spectral-light.woff2" 
+              as="font" 
+              type="font/woff2"
+              crossOrigin=""
+          />
+
+          <meta 
+              name="Description" 
+              content="The personal website of Mr. Jackson Breyer, software engineer and human being." 
+          />
         </Head>
         <body>
           <Main />
