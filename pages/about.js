031063be9965766e7647da17d7c77ef3f0af7a31
breyerjs <breyer.js@gmail.com>
2021-02-28 11:16:41 -0800

    new link styles

--- pages/about.js
+++ pages/about.js
@@ -18,22 +18,22 @@ const About = (props) => {
                 <div className={styles.aboutItems}>
                     <div className={styles.aboutSection}>
                         <div className={styles.heading}>Profiles:</div>
-                        <div className={styles.content}><a href="https://github.com/breyerjs" target="_blank">Github</a>, <a href="https://www.linkedin.com/in/breyerjs" target="_blank">Linkedin</a></div>
+                        <p className={styles.content}><a href="https://github.com/breyerjs" target="_blank">Github</a>, <a href="https://www.linkedin.com/in/breyerjs" target="_blank">Linkedin</a></p>
                     </div>
 
                     <div className={styles.aboutSection}>
                         <div className={styles.heading}>Site repo:</div>
-                        <div className={styles.content}><a href="https://github.com/breyerjs/jacksonbreyer/tree/master" target="_blank">View on Github</a></div>
+                        <p className={styles.content}><a href="https://github.com/breyerjs/jacksonbreyer/tree/master" target="_blank">View on Github</a></p>
                     </div>
 
                     <div className={styles.aboutSection}>
                         <div className={styles.heading}>Languages:</div>
-                        <div className={styles.content}>Many. Especially with a Python/Javascript focus. But I've worked all over the place.</div>
+                        <p className={styles.content}>Many. Especially with a Python/Javascript focus. But I've worked all over the place.</p>
                     </div>
 
                     <div className={styles.aboutSection}>
                         <div className={styles.heading}>Professional Interests:</div>
-                        <div className={styles.content}>Databases, Engineering Management and Leadership, Feature Analysis and Experimentation, Full-Stack Web Development, Natural Language Processing, Search</div>
+                        <p className={styles.content}>Databases, Engineering Management and Leadership, Feature Analysis and Experimentation, Full-Stack Web Development, Natural Language Processing, Search</p>
                     </div>
                 </div>
             </Layout>
